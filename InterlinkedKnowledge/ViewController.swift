da2d446b32020aedf40b7b55cd179fc41996dd12
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2024-03-19 13:24:48 +0500

    Initial Commit

---
+++ InterlinkedKnowledge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterlinkedKnowledge
+//
+//  Created by Bektemur Mamashayev on 19/3/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
